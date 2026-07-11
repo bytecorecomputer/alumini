1653f50581fcd8cfaaffc2c757aa24a6623ab677
coderafroj <coderafroj@users.noreply.github.com>
2026-07-11 13:59:17 -0400

    fix: remove CodeLabsNavbar import from Layout to resolve build error

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,22 +2,9 @@ import { useLocation } from 'react-router-dom';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import AdminBottomNav from './AdminBottomNav';
-import CodeLabsNavbar from './CodeLabsNavbar';
 
 export default function Layout({ children }) {
     const location = useLocation();
-    const isCodeLabs = location.pathname.startsWith('/coderafroj');
-
-    if (isCodeLabs) {
-        return (
-            <div className="min-h-screen flex flex-col bg-[#050505] overflow-hidden">
-                <CodeLabsNavbar />
-                <main className="flex-grow flex flex-col relative">
-                    {children}
-                </main>
-            </div>
-        );
-    }
 
     return (
         <div className="min-h-screen flex flex-col bg-gray-50 overflow-hidden md:pb-0 pb-[80px]">
